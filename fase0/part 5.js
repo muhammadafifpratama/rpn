112cac640d4751a5ea28cb918e30300d97a64a95
muhammadafifpratama <afifpratama@gmail.com>
2023-12-11 15:52:55 +0700

    first commit

---
+++ fase0/part 5.js
@@ -0,0 +1,32 @@
+// Kamu diminta untuk memprogram suatu game sederhana, Proxytia namanya. 
+// Untuk memulai game itu diperlukan 2 variabel (untuk sekarang), 
+// yaitu nama dan peran. Variabel peran harus memiliki isi data, 
+// bila nama kosong, pemain akan diberikan peringatan berupa "nama wajib diisi"
+// bila kosong pemain akan diberikan peringatan berupa "Pilih Peranmu untuk memulai game". 
+// Terdapat 3 peran berbeda yaitu Ksatria, Tabib, dan Penyihir. 
+// Tugas Anda adalah untuk membuat program yang mengecek isi variabel 
+// peran serta mengeluarkan respon sesuai isi variabel tersebut.
+//ada 4 jenis respons sesuai dengan 3 jenis peran yaitu
+//halo Ksatria ${nama} , kamu dapat menyerang dengan senjatamu!
+//halo Tabib ${nama} , kamu akan membantu temanmu yang terluka
+//halo Penyihir ${nama} , ciptakan keajaiban yang membantu kemenanganmu!
+//tapi kayaknya kamu jadi bot aja ya, peran yang kamu pilih ga ada
+//tips belajar penggunaan `` (backtick) pada javascript agar
+//mudah dalam memasukan variabel ke dalam string
+//tapi tanpa backtick juga ga masalah sih yg penting output sesuai
+
+// algoritma
+//isi algoritma mu disini (AWAS KALO GA DI ISI!!!!)
+//bingung algoritma yang kayak gimana wkwkwkwk semoga ini bener 
+//function(nama, peran)
+//if nama kosong print nama wajib diisi
+//if peran kosong print pilih peran
+//if peran ksatria print halo ksatria concat print nama
+//if peran tabib print halo tabib concat print nama
+//if peran penyihir print halo penyihir concat print nama
+//else halo nama kamu jadi bot aja ya 
+
+
+let nama = "", peran = "";
+
+//code disini gunakan console.log untuk outputnya
